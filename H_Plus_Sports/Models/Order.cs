15eed2c0f48bee255162ff0e6f246cc4d19ce89c
=Ümit Karaoğul <=ukaraogul@gmail.com>
2018-12-31 04:01:57 +0300

    DbContext added

---
+++ H_Plus_Sports/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H_Plus_Sports.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderItem = new HashSet<OrderItem>();
+        }
+
+        public int OrderId { get; set; }
+        public DateTime? Date { get; set; }
+        public decimal? TotalDue { get; set; }
+        public string Status { get; set; }
+        public int CustomerId { get; set; }
+        public int SalespersonId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Salesperson Salesperson { get; set; }
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+    }
+}
---
+++ H_Plus_Sports/Models/Salesperson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H_Plus_Sports.Models
+{
+    public partial class Salesperson
+    {
+        public Salesperson()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int SalespersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
